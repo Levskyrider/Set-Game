7b7439d58ae3a7bd8c5eb3215ef70d9da8614e78
levskyrider <levskyrider@gmail.com>
2024-12-18 00:16:57 +0200

    first commit

---
+++ Set Game/Set_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Set_GameApp.swift
+//  Set Game
+//
+//  Created by Dmitro Levkutnyk on 18.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Set_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
